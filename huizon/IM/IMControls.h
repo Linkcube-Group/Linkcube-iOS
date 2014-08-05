3c131213c554257bf3ae1a09f3a60f2b0df5c1b4
zhengfuzhe2013 <yy_you_xiang@163.com>
2014-08-05 16:53:57 +0800

    fix bug

--- huizon/IM/IMControls.h
+++ huizon/IM/IMControls.h
@@ -8,8 +8,39 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    NotificationTypeAddfriend = 0, //收到加好友的通知
+    NotificationTypeOther          //其他
+}NotificationType;
+
 @interface IMControls : NSObject
 
 +(IMControls *)defaultControls;
+
+//收到新消息
+-(void)receiveNewNoticesWithNotiType:(NotificationType)type;
+
+//获取新消息数
+-(NSInteger)getNewNoticesCountWithType:(NotificationType)type;
+
+//新消息数减1
+-(NSInteger)deleteOneNoticeCountWithType:(NotificationType)type;
+
+//清空消息数
+-(void)clearNewNoticesCountWithType:(NotificationType)type;
+
+
+
+//收到新聊天
+-(void)receiveNewMessageWithJidStr:(NSString *)jid;
+
+//获取新聊天数
+-(NSInteger)getNewMessageCountWithJidStr:(NSString *)jid;
+
+//新聊天数减1
+-(NSInteger)deleteOneMessageCountWithJidStr:(NSString *)jid;
+
+//清空聊天数
+-(void)clearNewMessageCountWithJidStr:(NSString *)jid;
 
 @end
--- huizon/IM/IMControls.m
+++ huizon/IM/IMControls.m
@@ -7,6 +7,9 @@
 //
 
 #import "IMControls.h"
+#import "XMPPvCardTemp.h"
+
+#define NOTICES_RECEIVE_ADDFRIEND_COUNT_NEW [self createPath:@"notifices_receive_count_new"]
 
 static IMControls * defaultManagerInstance = nil;
 
@@ -30,6 +33,108 @@ static IMControls * defaultManagerInstance = nil;
         //init code
     }
     return self;
+}
+
+- (NSString *)createPath:(NSString *)path
+{
+    if(theApp.isXmppAuthenticated)
+        return [NSString stringWithFormat:@"%@/%@", theApp.xmppvCardUser.nickname, path];
+    return nil;
+}
+
+#pragma mark
+#pragma mark - 收到新消息
+
+-(void)receiveNewNoticesWithNotiType:(NotificationType)type
+{
+    if(type == NotificationTypeAddfriend)
+    {
+        NSInteger count = [[NSUserDefaults standardUserDefaults] integerForKey:NOTICES_RECEIVE_ADDFRIEND_COUNT_NEW];
+        count++;
+        [[NSUserDefaults standardUserDefaults] setInteger:count forKey:NOTICES_RECEIVE_ADDFRIEND_COUNT_NEW];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+    
+}
+
+#pragma mark
+#pragma mark - 获取新消息数
+
+-(NSInteger)getNewNoticesCountWithType:(NotificationType)type
+{
+    if(type == NotificationTypeAddfriend)
+    {
+        NSInteger count = [[NSUserDefaults standardUserDefaults] integerForKey:NOTICES_RECEIVE_ADDFRIEND_COUNT_NEW];
+        return count>99?99:count;
+    }
+    return 0;
+}
+
+#pragma mark
+#pragma mark - 新消息数减1
+
+-(NSInteger)deleteOneNoticeCountWithType:(NotificationType)type
+{
+    NSInteger count = [[NSUserDefaults standardUserDefaults] integerForKey:NOTICES_RECEIVE_ADDFRIEND_COUNT_NEW];
+    if(count>0)
+        count--;
+    [[NSUserDefaults standardUserDefaults] setInteger:count forKey:NOTICES_RECEIVE_ADDFRIEND_COUNT_NEW];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    return count;
+}
+
+#pragma mark
+#pragma mark - 清空消息数
+
+-(void)clearNewNoticesCountWithType:(NotificationType)type
+{
+    if(type == NotificationTypeAddfriend)
+    {
+        [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:NOTICES_RECEIVE_ADDFRIEND_COUNT_NEW];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+}
+
+#pragma mark
+#pragma mark - 收到新聊天
+
+-(void)receiveNewMessageWithJidStr:(NSString *)jid
+{
+    NSInteger count = [[NSUserDefaults standardUserDefaults] integerForKey:[self createPath:jid]];
+    count++;
+    [[NSUserDefaults standardUserDefaults] setInteger:count forKey:[self createPath:jid]];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+#pragma mark
+#pragma mark - 获取新聊天数
+
+-(NSInteger)getNewMessageCountWithJidStr:(NSString *)jid
+{
+    NSInteger count = [[NSUserDefaults standardUserDefaults] integerForKey:[self createPath:jid]];
+    return count>99?99:count;
+}
+
+#pragma mark
+#pragma mark - 新聊天数减1
+
+-(NSInteger)deleteOneMessageCountWithJidStr:(NSString *)jid
+{
+    NSInteger count = [[NSUserDefaults standardUserDefaults] integerForKey:[self createPath:jid]];
+    if(count>0)
+        count--;
+    [[NSUserDefaults standardUserDefaults] setInteger:count forKey:[self createPath:jid]];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    return count;
+}
+
+#pragma mark
+#pragma mark - 清空聊天数
+
+-(void)clearNewMessageCountWithJidStr:(NSString *)jid
+{
+    [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:[self createPath:jid]];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 @end
