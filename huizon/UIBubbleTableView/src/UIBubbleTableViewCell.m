2e820814be9dac753ff83470dcf5e1514899a284
zhengfuzhe2013 <yy_you_xiang@163.com>
2014-08-08 03:32:07 +0800

    bug

--- huizon/UIBubbleTableView/src/UIBubbleTableViewCell.m
+++ huizon/UIBubbleTableView/src/UIBubbleTableViewCell.m
@@ -92,7 +92,7 @@
         CGFloat avatarX = (type == BubbleTypeSomeoneElse) ? 12 : self.frame.size.width - 52;
         CGFloat avatarY = self.frame.size.height - 50;
         
-        self.avatarImage.frame = CGRectMake(avatarX, avatarY + 8, 40, 40);
+        self.avatarImage.frame = CGRectMake(avatarX, avatarY + 14, 40, 40);
         [self addSubview:self.avatarImage];
         
         CGFloat delta = self.frame.size.height - (self.data.insets.top + self.data.insets.bottom + self.data.view.frame.size.height);
